af812988a65002901b5a94561d1a4251b1b7c04b
Dzmitry Struneuski <isidbrest@gmail.com>
2024-06-21 00:59:49 +0300

    add all files

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# psremoteinstaller
-automates the installation of any application across multiple remote computers in a local network usin PowerShell
+Script Description
+
+This PowerShell script automates the installation of any app across multiple remote computers in a local network. 
+It reads a list of target computers from a specified file, copies the app installer to each remote computer, and installs app silently using a scheduled task with system privileges. 
+After the installation, it cleans up by removing the temporary files and folders. 
+The script ensures smooth installation by checking for connectivity issues and handling errors gracefully.
+
+
+Explanation:
+Local path to the Skype installer file:
+C:\Users\usernameexample\Documents\Skype-8.122.0.205.exe specifies where the installer is located locally on your machine.
+
+Reading the list of computers:
+Reads the list of target computers from C:\Users\usernameexample\Documents\computers.txt.
+
+Command to install Skype:
+Specifies the installation command to execute the Skype installer silently.
+
+Prompt for credentials:
+Prompts user for credentials once, which will be used for remote connections.
+
+Function to install Skype on a remote computer:
+Checks if the computer is accessible: Verifies that the target computer is reachable.
+
+Creates a session: Establishes a session to the target computer using the provided credentials.
+
+Creates the directory on the remote computer: If the directory does not exist, it creates it.
+
+Copies the installer file to the remote computer: Transfers the Skype installer to the remote computer.
+
+Creates and starts a scheduled task on the remote computer to install Skype: Creates and runs a scheduled task with system privileges to perform the Skype installation.
+
+Waits for the task to complete: Continuously checks if the task has finished running.
+
+Removes the temporary folder: Deletes the temporary directory after the installation is complete.
+
+Iterates over all computers and installs Skype:
+
+Loops through each computer listed in the computers.txt, and runs the Install-App function.
---
+++ psremoteinstaller.ps1
@@ -0,0 +1,85 @@
+﻿# Local path to the App installer file
+$LocalAppInstaller = "C:\Users\usernameexample\Documents\Skype-8.122.0.205.exe"
+$RemoteInstallerPath = "C:\Temp\AppSetup.exe"
+
+# Reading the list of computers
+$computers = Get-Content -Path "C:\Users\usernameexample\Documents\computers.txt"
+
+# Command to install App
+$taskName = "InstallApp"
+$installCommand = "$RemoteInstallerPath /VERYSILENT /NORESTART"
+
+# Prompt for credentials once
+$cred = Get-Credential
+
+# Function to install App on a remote computer
+function Install-App {
+    param (
+        [string] $ComputerName,
+        $credential
+    )
+
+    # Check if the computer is accessible
+    if (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet) {
+        Write-Host "Connecting to $ComputerName..."
+
+        try {
+            # Create a session
+            $session = New-PSSession -ComputerName $ComputerName -Credential $credential
+
+            # Create the directory on the remote computer, if it does not exist
+            Invoke-Command -Session $session -ScriptBlock {
+                param ($RemoteInstallerPath)
+                $folderPath = [System.IO.Path]::GetDirectoryName($RemoteInstallerPath)
+                if (-Not (Test-Path -Path $folderPath)) {
+                    New-Item -Path $folderPath -ItemType Directory | Out-Null
+                }
+            } -ArgumentList $RemoteInstallerPath
+
+            # Copy the installer file to the remote computer
+            Copy-Item -Path $LocalAppInstaller -Destination $RemoteInstallerPath -ToSession $session -Force
+
+            # Create a scheduled task on the remote computer to install App
+            $scriptBlock = {
+                param ($installCommand, $taskName, $RemoteInstallerPath)
+
+                # Command to create the scheduled task
+                $action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c $installCommand"
+                $trigger = New-ScheduledTaskTrigger -AtStartup
+                $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+                Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName $taskName
+
+                # Start the scheduled task
+                Start-ScheduledTask -TaskName $taskName
+                Write-Host "Scheduled task for App installation created and started on $ComputerName"
+
+                # Wait for the task to complete
+                while ((Get-ScheduledTask $taskName).State -eq 'Running') {
+                    Start-Sleep -Seconds 5
+                }
+
+                # Remove the temporary folder
+                $folderPath = [System.IO.Path]::GetDirectoryName($RemoteInstallerPath)
+                Remove-Item -Path $folderPath -Recurse -Force
+                Write-Host "Temp folder removed on $ComputerName"
+            }
+
+            # Execute the command on the remote computer
+            Invoke-Command -Session $session -ScriptBlock $scriptBlock -ArgumentList $installCommand, $taskName, $RemoteInstallerPath
+
+            # Close the session
+            Remove-PSSession -Session $session
+        }
+        catch {
+            Write-Error "Failed to copy installer or connect to $ComputerName. Error: $_"
+        }
+    } else {
+        Write-Host "$ComputerName is not accessible."
+    }
+}
+
+# Iterate over all computers and install App
+foreach ($computer in $computers) {
+    Write-Host "Updating App on $computer"
+    Install-App -ComputerName $computer -credential $cred
+}
